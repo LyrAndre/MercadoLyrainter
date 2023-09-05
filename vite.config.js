0ccb24d5a7e9538046ad63bc80f784ec2de7d95f
LyrAndre <andre11522@hotmail.com>
2023-09-05 20:45:00 -0300

    primeiro commit funcional do mercado lyra

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import tailwindcss from "tailwindcss";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/MercadoLyrainter/",
+  plugins: [],
+  resolve: {},
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./index.html"),
+        checkout: resolve(__dirname, "./checkout.html"),
+        pedidos: resolve(__dirname, "./pedidos.html"),
+      },
+    },
+  },
+});
+  
